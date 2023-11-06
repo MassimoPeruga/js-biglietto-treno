0a2160cf75c1f3c45b787188d1c52ca8240e911e
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-06 17:37:05 +0100

    fix: verica dei numeri inseriti

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Il programma tramite prompt deve:
 1. Chiedere all'utente il numero di kilometri che vuole percorrere e salvarlo in una variabile;
 2. Chiedere all'utente la sua età e sarlvarla in una variabile;
-3. Calcolare il prezzo del biglietto moltiplicando il numero dei kilometri da percorrere per 0.21;
-4. Porre una prima condizione per la quale applicare lo sconto del 20% ai minorenni;
-5. Porre una seconda condizione per la quale applicare lo sconto del 40% agli over 65;
-6. Comunicare il prezzo finale.
+3. Verifica se i valori inseriti sono numeri validi;
+4. Calcolare il prezzo del biglietto moltiplicando il numero dei kilometri da percorrere per 0.21;
+5. Porre una prima condizione per la quale applicare lo sconto del 20% ai minorenni;
+6. Porre una seconda condizione per la quale applicare lo sconto del 40% agli over 65;
+7. Comunicare il prezzo finale.
--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 'use strict';
 // 1. Chiedi all'utente il numero di chilometri da percorrere
-const distanza = prompt("Inserisci il numero dei kilometri che vuoi percorrere:");
+const distanza = parseInt(prompt("Inserisci il numero dei kilometri che vuoi percorrere:"));
 // 2. Chiedi all'utente la sua età
-const etaUtente = prompt("Insterisci la tua età:");
-// 3. Calcola il prezzo del biglietto
-let prezzo = distanza * 0.21;
-// 4. Applica lo sconto in determinate condizioni
-if (etaUtente < 18) {
-    prezzo *= 0.8;
-} else if (etaUtente >= 65) {
-    prezzo *= 0.6;
+const etaUtente = parseInt(prompt("Insterisci la tua età:"));
+// 3. Verifica se il valore inserito per i chilometri è un numero valido
+if (isNaN(distanza) || isNaN(etaUtente)) {
+    alert("Non hai inserito un numero, ricarica la pagina per riprovare.")
+} else {
+    // 4. Calcola il prezzo del biglietto
+    let prezzo = distanza * 0.21;
+    // 5. Applica lo sconto in determinate condizioni
+    if (etaUtente < 18) {
+        prezzo *= 0.8;
+    } else if (etaUtente >= 65) {
+        prezzo *= 0.6;
+    }
+    // 6. Comunica il prezzo finale
+    console.log("Il prezzo del biglietto è: " + prezzo.toFixed(2) + " euro.");
+    alert("Il prezzo del biglietto è: " + prezzo.toFixed(2) + " euro.");
 }
-// 6. Comunica il prezzo finale
-console.log("Il prezzo del biglietto è: " + prezzo.toFixed(2) + " euro.");
-alert("Il prezzo del biglietto è: " + prezzo.toFixed(2) + " euro.");
